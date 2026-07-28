fea2f9c739b279f0f743f05276d86196f4028296
gpzz9trkdb-cloud <gpzz9trkdb@privaterelay.appleid.com>
2026-07-28 22:55:21 +0200

    Einladungskarte in Umschlag integriert

--- script.js
+++ script.js
@@ -1,31 +1,10 @@
 const openButton = document.getElementById("openInvitation");
 
-const envelopePage = document.querySelector(".envelope-container");
-
-const invitationCard = document.querySelector(".invitation-card");
-
 const envelope = document.querySelector(".envelope");
 
 
 openButton.addEventListener("click", function () {
 
     envelope.classList.add("open");
-
-
-    setTimeout(function () {
-
-        envelopePage.style.opacity = "0";
-
-    }, 900);
-
-
-    setTimeout(function () {
-
-        envelopePage.style.display = "none";
-
-        invitationCard.classList.remove("hidden");
-
-    }, 1000);
-
 
 });
