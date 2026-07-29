b8db8f6505758f643509940fd7e74d5d62585367
gpzz9trkdb-cloud <gpzz9trkdb@privaterelay.appleid.com>
2026-07-29 09:20:16 +0200

    alles neu

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-const openButton = document.getElementById("openInvitation");
+const button = document.getElementById("openButton");
+const letter = document.querySelector(".letter");
+const envelope = document.querySelector(".envelope");
+const hero = document.querySelector(".hero");
 
-const envelopePage = document.querySelector(".envelope-container");
+button.addEventListener("click", () => {
 
-const invitationCard = document.querySelector(".invitation-card");
+    button.disabled = true;
 
+    // Brief fährt langsam heraus
+    letter.animate(
+        [
+            {
+                transform: "translateX(-50%) translateY(0px)",
+                opacity: 1
+            },
+            {
+                transform: "translateX(-50%) translateY(-120px)",
+                opacity: 1
+            }
+        ],
+        {
+            duration: 1200,
+            easing: "ease-in-out",
+            fill: "forwards"
+        }
+    );
 
-openButton.addEventListener("click", function () {
+    // Danach vergrößert sich der Brief
+    setTimeout(() => {
 
-    envelopePage.style.display = "none";
+        letter.style.transition = "all 0.9s ease";
 
-    invitationCard.classList.remove("hidden");
+        letter.style.position = "fixed";
+        letter.style.left = "50%";
+        letter.style.top = "50%";
+        letter.style.bottom = "auto";
+
+        letter.style.transform = "translate(-50%, -50%)";
+
+        letter.style.width = "90vw";
+        letter.style.maxWidth = "700px";
+        letter.style.aspectRatio = "auto";
+        letter.style.height = "80vh";
+        letter.style.zIndex = "100";
+
+    }, 1200);
 
 });
